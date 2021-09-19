a2838f1ebdc2d3e8323c7be8b5393b572844c133
Nikegamerjjjj <myftr12394@gmail.com>
2021-09-19 19:39:09 +0200

    updates!

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 
-let score = 0;
+let plants = 0;
 
 let cursorCost = 15;
 let cursors = 0;
 
-let gardenerCost = 35;
+let gardenerCost = 50;
 let gardeners = 0;
 
-
-
-
+let fertilizerCost = 150;
+let fertilizer = 0;
 
 
 function buyCursor() {
-	if (score >= cursorCost) {
-		score = score - cursorCost;
+	if (plants >= cursorCost) {
+		plants = plants - cursorCost;
 		cursors = cursors + 1;
 		cursorCost = Math.round(cursorCost * 1.15);
 
 		
 
-		document.getElementById("score").innerHTML = score;
+		document.getElementById("plants").innerHTML = plants;
 		document.getElementById("cursorcost").innerHTML = cursorCost;
 		document.getElementById("cursors").innerHTML = cursors;
 	}
@@ -29,29 +28,47 @@ function buyCursor() {
 
 
 function buyGardener() {
-	if (score >= gardenerCost) {
-		score = score - gardenerCost;
+	if (plants >= gardenerCost) {
+		plants = plants - gardenerCost;
 		gardeners = gardeners + 1;
 		gardenerCost = Math.round(gardenerCost * 1.15);
 
-		document.getElementById("score").innerHTML = score;
+		document.getElementById("plants").innerHTML = plants;
 		document.getElementById("gardenercost").innerHTML = gardenerCost;
 		document.getElementById("gardeners").innerHTML = gardeners;
 	}
 }
 
+
+function buyFertilizer() {
+	if (plants >= fertilizerCost) {
+		plants = plants - fertilizerCost;
+		fertilizer = fertilizer + 1;
+		fertilizerCost = Math.round(fertilizerCost * 1.15);
+
+		document.getElementById("plants").innerHTML = plants;
+		document.getElementById("fertilizercost").innerHTML = fertilizerCost;
+		document.getElementById("fertilizers").innerHTML = gardeners;
+	}
+}
+
+
+
+
+
+
 document.getElementById("btn").onclick = function () {
-	score += 1;
+	plants += 1;
 	
-	document.getElementById("score").innerHTML = score;
+	document.getElementById("plants").innerHTML = plants;
 }
 
 
 setInterval(function() {
-	score = score + cursors;
-	score = score + gardeners * 5;
-	
-	document.getElementById("score").innerHTML = score;
+	plants = plants + cursors;
+	plants = plants + gardeners * 5;
+	plants = plants + fertilizer * 13;
+	document.getElementById("plants").innerHTML = plants;
 }, 1500);
 
 
